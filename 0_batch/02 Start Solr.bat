9b9ccde4e655a19fefb37a13ebaf3dd94419b369
Ee-Cs <53374479+Ee-Cs@users.noreply.github.com>
2025-03-13 14:27:43 +0100

    Delete 0_batch directory

--- 0_batch/02 Start Solr.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set JAVA_HOME=C:\PROGRA~1\JAVA\JDK-23
-
-:: This Solr version runs without errors. Solr version 9.8.1 failed.
-set SOLR_DIR=D:\TOOLS\SOLR-9.6.0
-
-%SOLR_DIR%\bin\solr.cmd start -c -f -Dsolr.modules=sql
-pause
